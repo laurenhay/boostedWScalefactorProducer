3541d7631e3b6c229ea06c4afd84f073b6202afb
kaustuvdatta <kaustuv.datta@cern.ch>
2020-07-15 03:24:28 +0200

    crab config (placeholder)

--- Skimmer/test/crabconfig_2018MC_NANO.py
+++ Skimmer/test/crabconfig_2018MC_NANO.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: crabconfig_2018MC -s NANO --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102x_upgrade2018_realisitic_v19-v1 --no_exec --era Run2_2018,run2_nanoAOD_102Xv1
+# with command line options: crabconfig_2018MC -s NANO --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102x_upgrade2018_realisitic_v19-v1 --no_exec --era Run2_2018,run2_nanoAOD_102Xv1 -n 100 --nThreads 8 --filein /store/mc/RunIIAutumn18NanoAODv5/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano1June2019_102X_upgrade2018_realistic_v19-v1/60000/8ABD924A-A197-6848-8949-A5539DC012C3.root
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
@@ -22,12 +22,12 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(100)
 )
 
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:crabconfig_2018MC_PAT.root'),
+    fileNames = cms.untracked.vstring('/store/mc/RunIIAutumn18NanoAODv5/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano1June2019_102X_upgrade2018_realistic_v19-v1/60000/8ABD924A-A197-6848-8949-A5539DC012C3.root'),
     secondaryFileNames = cms.untracked.vstring()
 )
 
@@ -37,7 +37,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('crabconfig_2018MC nevts:1'),
+    annotation = cms.untracked.string('crabconfig_2018MC nevts:100'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -70,6 +70,10 @@ process.NANOAODSIMoutput_step = cms.EndPath(process.NANOAODSIMoutput)
 process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step,process.NANOAODSIMoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(8)
+process.options.numberOfStreams=cms.untracked.uint32(0)
 
 # customisation of the process.
 
