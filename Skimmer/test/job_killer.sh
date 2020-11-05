32c8ff3018e738d0d6af774b4cae46efa15da5a8
kaustuvdatta <kaustuv.datta@cern.ch>
2020-11-05 15:07:24 +0100

    addl files

---
+++ Skimmer/test/job_killer.sh
@@ -0,0 +1,27 @@
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_B_v01
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_F_v01
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2017UL_mu_customiso_test
+
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_B_v01_test  
+
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_F_v1                                 
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2017UL_v01
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_B_v1
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2018RR_A_106X_test           
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2017UL_v01_test
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_C_v01
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTJets-amcatnloFXFX-pythia8_2017UL_mu_customIso_test  
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2017UL_v1
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_C_v1        
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTJets-amcatnloFXFX-pythia8_2017UL_v01                
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2018_mu_customIso_test_rereco
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_D_v01       
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTJets-amcatnloFXFX-pythia8_2017UL_v1                 
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2018_v01
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_D_v1        
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTTo2L2Nu-powheg-pythia8_2017UL_v01                   
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2018_v01_test
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_E_v01       
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTTo2L2Nu-powheg-pythia8_2017UL_v1
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_SingleMuon2017UL_E_v1
+crab kill crab_projects/crab_boostedWtaggingSF_Skims_TTToSemiLeptonic-powheg-pythia8_2017UL_106X_v00
