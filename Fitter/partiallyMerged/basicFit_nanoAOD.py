136a28df280ab8be061f73bb33f3f4d5bc21da04
laurenhay <lmhay@buffalo.edu>
2022-05-06 08:06:14 -0500

    final codes corresp. to README

--- Fitter/partiallyMerged/basicFit_nanoAOD.py
+++ Fitter/partiallyMerged/basicFit_nanoAOD.py
@@ -1,17 +1,29 @@
+from optparse import OptionParser
+
 import ROOT
-from ROOT import *
-#DON'T USE FUNCTIONS, WE BREAK LIKE MEN
+
 #Currently compatible with CMSSW_12_0
 #run with python3
 
 #open workspace written by makeRooDataset.py and load RooDataSet
-filename = "workspace.root"
+
+parser = OptionParser()
+
+parser.add_option('--workspace', action="store",type="string",dest="workspace",default="workspace", help="Name of workspace")
+parser.add_option('--mass_var', action="store",type="string",dest="mass_var",default="", help="jet mass grooming variable")
+
+(options, args) = parser.parse_args()
+
+
+print(options.workspace)
+filename = options.workspace.replace(".root","")+".root"
+
 try: 
     os.stat(filename)
     print("Workspace root file found.")
 except:
     print("Workspace root file now found -- please run makeRooDataset first")
-f = TFile(filename)
+f = ROOT.TFile(filename)
 workspace = f.Get("workspace")
 
 #total dataset
@@ -25,16 +37,85 @@ rdataset_data_pass_mj = workspace.data("rdataset_data_passtau21cut_mj")
 rdataset_mc_pass_mj = workspace.data("rdataset_totalMC_passtau21cut_mj")
 
 rrv_mass_j  = workspace.var("rrv_mass_j")
+#set signal range
+rrv_mass_j.setRange("signal_region",65,105)
 
 #convert mc to histpdf for plotting
 hist_mc = rdataset_mc_pass_mj.binnedClone()
-histpdf_mc = ROOT.RooHistPdf(rdataset_mc_pass_mj.GetName()+"_histpdf", rdataset_data_pass_mj.GetName()+"_histpdf", RooArgSet(rrv_mass_j), hist_mc)
+histpdf_mc = ROOT.RooHistPdf(rdataset_mc_pass_mj.GetName()+"_histpdf", rdataset_data_pass_mj.GetName()+"_histpdf", ROOT.RooArgSet(rrv_mass_j), hist_mc)
 
 
 #make frame for data
-frame_mj = rrv_mass_j.frame(RooFit.Title("MC and Data fits PASS"),ROOT.RooFit.Bins(int(rrv_mass_j.getBins())) )
+frame_mj = rrv_mass_j.frame(ROOT.RooFit.Title("Fits for Jet Mass "+options.mass_var),ROOT.RooFit.Bins(int(rrv_mass_j.getBins())) )
+
+rrv_scale_number_mc = ROOT.RooRealVar("rrv_scale_number", "rrv_scale_number", (rdataset_mc_pass_mj.sumEntries()/rdataset_data_pass_mj.sumEntries()))
+print(rdataset_mc_pass_mj.sumEntries())
+print(rdataset_data_pass_mj.sumEntries())
+#rescale
+scale_number_mc = rrv_scale_number_mc.getValV()
+#plot data
+rdataset_data_pass_mj.plotOn(frame_mj, ROOT.RooFit.Name("data"))
+#plot mc normalized to data
+histpdf_mc.plotOn(frame_mj, ROOT.RooFit.Name("MC"), ROOT.RooFit.MoveToBack(), ROOT.RooFit.DrawOption("F"), ROOT.RooFit.FillColor(414),ROOT.RooFit.LineColor(ROOT.kBlack))
+histpdf_mc.plotOn(frame_mj, ROOT.RooFit.Name("MC_invisible"), ROOT.RooFit.LineColor(ROOT.kBlack), ROOT.RooFit.LineWidth(2))
+
+#model_histpdf.plotOn(frame_mj) #, ROOT.RooFit.Normalization(scale_number_mc), ROOT.RooFit.DrawOption("F"), ROOT.RooFit.FillColor(kMagenta))
+#make frame for pdf before fit
+frame_mj_pdf = rrv_mass_j.frame(ROOT.RooFit.Title("Gauss PDF before fit"))
+
+def makePDF(workspace, label):
+    #now we make our pdf model --> for simplicity for now double gaussian
+    rrv_mean1_gaus  = ROOT.RooRealVar("rrv_mean1_gaus_"+label, "rrv_mean1_gaus_"+label, 80.,65.,100.)
+    rrv_sigma1_gaus = ROOT.RooRealVar("rrv_sigma1_gaus_"+label ,"rrv_sigma1_gaus_"+label ,10,5.,30.)
+    #rrv_mean2_gaus  = ROOT.RooRealVar("rrv_mean2_gaus"+label ,"rrv_mean2_gaus"+label ,170,150,180)
+    #rrv_sigma2_gaus = ROOT.RooRealVar("rrv_sigma2_gaus"+label ,"rrv_sigma2_gaus"+label ,13,5.,30.)
+    gaus1 = ROOT.RooGaussian("gaus1_"+label ,"gaus1_"+label ,rrv_mass_j,rrv_mean1_gaus,rrv_sigma1_gaus)     
+    #gaus2 = ROOT.RooGaussian("gaus2"+label ,"gaus2"+label ,rrv_mass_j,rrv_mean2_gaus,rrv_sigma2_gaus)
+    
+    #adding an exponential
+    rrv_const = ROOT.RooRealVar("rrv_const_"+label, "rrv_const_"+label, -2.0e-02, -1., 1.)
+    exp = ROOT.RooExponential("exp"+label, "exp"+label, rrv_mass_j, rrv_const)
+    
+
+
+    rrv_frac1 = ROOT.RooRealVar("rrv_frac1_"+label ,"rrv_frac1_"+label ,0.99,0.,1.)
+#    rrv_frac2 = ROOT.RooRealVar("rrv_frac2"+label, "rrv_frac2"+label, 0.10,0.,1.)
+    
+#    model_pdf = ROOT.RooAddPdf("model_pdf"+label,"model_pdf"+label,ROOT.RooArgList(gaus1,gaus2, exp),ROOT.RooArgList(rrv_frac1, rrv_frac2),1)
+    model_pdf = gaus1
+#    model_pdf = ROOT.RooAddPdf("model_pdf_"+label,"model_pdf_"+label,ROOT.RooArgList(gaus1,exp),ROOT.RooArgList(rrv_frac1),1)
+    #Now make extended model
+    print("Making extended model")
+    rrv_number = ROOT.RooRealVar("rrv_number","number of events",500.,0.,1e5)
+    model_extended = ROOT.RooExtendPdf("model_extendedpdf_"+label, "model_extendedpdf_"+label, model_pdf, rrv_number) #what should rrv number be? complicated in 
+    
+    #add pdf to workspace
+    getattr(workspace, 'import')(model_extended)
+    return workspace.pdf("model_extendedpdf_"+label)
+
+#make models
+model_extended_data = makePDF(workspace, "data") 
+model_extended_mc = makePDF(workspace, "allMC")
+
+#plot model before fit
+model_extended_mc.plotOn(frame_mj_pdf)
+
+#do fits
+fitResult_data_pass = model_extended_data.fitTo(rdataset_data_pass_mj, ROOT.RooFit.Save(1), ROOT.RooFit.Extended(1), ROOT.RooFit.Verbose(0), ROOT.RooFit.Range("signal_region"))
+fitResult_mc_pass = model_extended_mc.fitTo(rdataset_mc_pass_mj, ROOT.RooFit.Save(1), ROOT.RooFit.Extended(1), ROOT.RooFit.Verbose(0), ROOT.RooFit.Range("signal_region"))
+fitResult_data_pass.Print()
+fitResult_mc_pass.Print()
+
+#plot fit results on same frame as data and MC
+model_extended_data.plotOn(frame_mj,ROOT.RooFit.Name("Data fit"), ROOT.RooFit.LineStyle(ROOT.kSolid), ROOT.RooFit.LineColor(ROOT.kRed))
+model_extended_mc.plotOn(frame_mj, ROOT.RooFit.Name("MC fit"), ROOT.RooFit.LineStyle(ROOT.kSolid),ROOT.RooFit.LineColor(ROOT.kBlue))
+#plot fit errors
+#model_extended_data.plotOn(frame_mj, ROOT.RooFit.DrawOption("F"), ROOT.RooFit.VisualizeError(fitResult_data_pass,1), ROOT.RooFit.Name("Fit error"),ROOT.RooFit.FillColor(kGray))
+#model_extended_mc.plotOn(frame_mj, ROOT.RooFit.DrawOption("F"), ROOT.RooFit.VisualizeError(fitResult_mc_pass,1), ROOT.RooFit.Name("Fit error"))
+
+
 #make legend
-legend = TLegend(0.6010112,0.7183362,0.8202143,0.919833)
+legend = ROOT.TLegend(0.6010112,0.7183362,0.8202143,0.919833)
 legend.SetTextSize(0.032)
 legend.SetLineColor(0)
 legend.SetShadowColor(0)
@@ -43,81 +124,65 @@ legend.SetLineWidth(1)
 legend.SetFillColor(0)
 legend.SetFillStyle(0)
 legend.SetMargin(0.35)
-
-rrv_scale_number_mc = ROOT.RooRealVar("rrv_scale_number", "rrv_scale_number", (rdataset_mc_pass_mj.sumEntries()/rdataset_data_pass_mj.sumEntries()))
-print(rdataset_mc_pass_mj.sumEntries())
-print(rdataset_data_pass_mj.sumEntries())
-#rescale
-scale_number_mc = rrv_scale_number_mc.getValV()
-#plot data
-rdataset_data_pass_mj.plotOn(frame_mj)
-#plot mc normalized to data
-histpdf_mc.plotOn(frame_mj, ROOT.RooFit.MoveToBack(), ROOT.RooFit.DrawOption("F"), ROOT.RooFit.FillColor(414),ROOT.RooFit.LineColor(ROOT.kBlack))
-#model_histpdf.plotOn(frame_mj) #, RooFit.Normalization(scale_number_mc), RooFit.DrawOption("F"), RooFit.FillColor(kMagenta))
-#make frame for pdf before fit
-frame_mj_pdf = rrv_mass_j.frame(RooFit.Title("DoubleGauss PDF before fit"))
-
-def makePDF(workspace, label):
-    #now we make our pdf model --> for simplicity for now double gaussian
-    rrv_mean1_gaus  = RooRealVar("rrv_mean1_gaus_"+label, "rrv_mean1_gaus"+label, 80.,65.,100.)
-    rrv_sigma1_gaus = RooRealVar("rrv_sigma1_gaus"+label ,"rrv_sigma1_gaus"+label ,10,5.,30.)
-    rrv_mean2_gaus  = RooRealVar("rrv_mean2_gaus"+label ,"rrv_mean2_gaus"+label ,170,150,180)
-    rrv_sigma2_gaus = RooRealVar("rrv_sigma2_gaus"+label ,"rrv_sigma2_gaus"+label ,13,5.,30.)
-    gaus1 = RooGaussian("gaus1"+label ,"gaus1"+label ,rrv_mass_j,rrv_mean1_gaus,rrv_sigma1_gaus)     
-    gaus2 = RooGaussian("gaus2"+label ,"gaus2"+label ,rrv_mass_j,rrv_mean2_gaus,rrv_sigma2_gaus)
-    #adding an exponential
-    rrv_const = RooRealVar("rrv_const"+label, "rrv_const"+label, -2.0e-02, -1., 1.)
-    exp = RooExponential("exp"+label, "exp"+label, rrv_mass_j, rrv_const)
+legend.AddEntry(frame_mj.findObject("data")       ,"Data"                ,"PLE");
+legend.AddEntry(frame_mj.findObject("MC")       ,"MC"                ,"F");
+legend.AddEntry(frame_mj.findObject("Data fit")   ,"Data fit"            ,"L");
+legend.AddEntry(frame_mj.findObject("MC fit")     ,"MC fit"              ,"L");
+frame_mj.addObject(legend)
 
 
-    rrv_frac1 = RooRealVar("rrv_frac1"+label ,"rrv_frac1"+label ,0.75,0.,1.)
-    rrv_frac2 = RooRealVar("rrv_frac2"+label, "rrv_frac2"+label, 0.20,0.,1.)
-    
-    model_pdf = RooAddPdf("model_pdf"+label,"model_pdf"+label,RooArgList(gaus1,gaus2, exp),RooArgList(rrv_frac1, rrv_frac2),1)
-    
-    #Now make extended model
-    print("Making extended model")
-    rrv_number = RooRealVar("rrv_number","number of events",500.,0.,1e5)
-    model_extended = RooExtendPdf("model_extendedpdf_"+label, "model_extendedpdf_"+label, model_pdf, rrv_number) #what should rrv number be? complicated in 
-    
-    #add pdf to workspace
-    getattr(workspace, 'import')(model_extended)
-    return workspace.pdf("model_extendedpdf_"+label)
 
-    #plot model before fit
-    model_extended.plotOn(frame_mj_pdf)
+rrv_mean_gaus_mc = workspace.var("rrv_mean1_gaus_allMC")
+rrv_sigma_gaus_mc = workspace.var("rrv_sigma1_gaus_allMC")
+rrv_mean_gaus_data = workspace.var("rrv_mean1_gaus_data")
+rrv_sigma_gaus_data = workspace.var("rrv_sigma1_gaus_data")
 
-#do fit
-model_extended_data = makePDF(workspace, "data") 
-model_extended_mc = makePDF(workspace, "allMC")
+data_over_mc_mean = rrv_mean_gaus_data.getVal()/rrv_mean_gaus_mc.getVal()
+data_over_mc_mean_error = data_over_mc_mean*((rrv_mean_gaus_data.getError()/rrv_mean_gaus_data.getVal())**2+(rrv_mean_gaus_mc.getError()/rrv_mean_gaus_mc.getVal())**2 )**0.5
 
-fitResult_data_pass = model_extended_data.fitTo(rdataset_data_pass_mj, RooFit.Save(1), RooFit.Extended(1), RooFit.Verbose(1))
-fitResult_mc_pass = model_extended_mc.fitTo(rdataset_mc_pass_mj, RooFit.Save(1), RooFit.Extended(1), RooFit.Verbose(1))
-fitResult_data_pass.Print()
-fitResult_mc_pass.Print()
+data_over_mc_sigma = rrv_sigma_gaus_data.getVal()/rrv_sigma_gaus_mc.getVal()
+data_over_mc_sigma_error = data_over_mc_sigma*((rrv_sigma_gaus_data.getError()/rrv_sigma_gaus_data.getVal())**2+(rrv_sigma_gaus_mc.getError()/rrv_sigma_gaus_mc.getVal())**2 )**0.5
 
-#plot fit results on same frame as data and MC
-model_extended_data.plotOn(frame_mj,ROOT.RooFit.LineStyle(ROOT.kSolid), ROOT.RooFit.LineColor(ROOT.kRed))
-model_extended_mc.plotOn(frame_mj, ROOT.RooFit.LineStyle(ROOT.kSolid),ROOT.RooFit.LineColor(ROOT.kBlue))
-#plot fit errors
-#model_extended_data.plotOn(frame_mj, RooFit.DrawOption("F"), RooFit.VisualizeError(fitResult_data_pass,1), RooFit.Name("Fit error"),RooFit.FillColor(kGray))
-#model_extended_mc.plotOn(frame_mj, RooFit.DrawOption("F"), RooFit.VisualizeError(fitResult_mc_pass,1), RooFit.Name("Fit error"))
+print("JMS (data/MC) = ", data_over_mc_mean, " +/- ", data_over_mc_mean_error)
+print("JMR (data/MC) = ", data_over_mc_sigma, " +/- ", data_over_mc_sigma_error)
 
+#draw fit parameters
+tl_MC_mean  = ROOT.TLatex(0.75,0.62, ("#mu_{MC } = %3.2f #pm %2.2f")%(rrv_mean_gaus_mc.getVal(), rrv_mean_gaus_mc.getError()) );
+tl_MC_sigma = ROOT.TLatex(0.75,0.57, ("#sigma_{MC }= %2.2f #pm %2.2f")%(rrv_sigma_gaus_mc.getVal(), rrv_sigma_gaus_mc.getError()) );
+tl_MC_mean.SetNDC(); tl_MC_sigma.SetNDC();
+tl_MC_mean.SetTextSize(0.03)
+tl_MC_sigma.SetTextSize(0.03)
+frame_mj.addObject(tl_MC_mean);
+frame_mj.addObject(tl_MC_sigma);
 
-#calculate efficiences
+tl_data_mean  = ROOT.TLatex(0.75,0.52, ("#mu_{data } = %3.2f #pm %2.2f")%(rrv_mean_gaus_data.getVal(), rrv_mean_gaus_data.getError()) );
+tl_data_sigma = ROOT.TLatex(0.75,0.47, ("#sigma_{data }= %2.2f #pm %2.2f")%(rrv_sigma_gaus_data.getVal(), rrv_sigma_gaus_data.getError()) );
+tl_data_mean.SetNDC(); tl_data_sigma.SetNDC();
+tl_data_mean.SetTextSize(0.03)
+tl_data_sigma.SetTextSize(0.03)
+frame_mj.addObject(tl_data_mean);
+frame_mj.addObject(tl_data_sigma);
+
+tl_scale  = ROOT.TLatex(0.75,0.42, ("JMS_{data/MC} = %3.2f #pm %2.2f")%(data_over_mc_mean, data_over_mc_mean_error) );
+tl_resolution = ROOT.TLatex(0.75,0.37, ("JMR_{data/MC}= %2.2f #pm %2.2f")%(data_over_mc_sigma, data_over_mc_sigma_error) );
+tl_scale.SetNDC(); tl_resolution.SetNDC();
+tl_scale.SetTextSize(0.03)
+tl_resolution.SetTextSize(0.03)
+frame_mj.addObject(tl_scale);
+frame_mj.addObject(tl_resolution);
 
 
 #draw frames on canvas
-c = TCanvas("dataTest", "dataTest", 800, 400)
-c.Divide(2)
-c.cd(1)
-gPad.SetLeftMargin(0.15)
+c = ROOT.TCanvas("dataTest", "dataTest", 1000, 800)
+#c.Divide(2)
+#c.cd(1)
+ROOT.gPad.SetLeftMargin(0.15)
 frame_mj.GetYaxis().SetTitleOffset(1.6)
 frame_mj.Draw()
-c.cd(2)
-gPad.SetLeftMargin(0.15)
-frame_mj_pdf.GetYaxis().SetTitleOffset(1.6)
-frame_mj_pdf.Draw()
+# c.cd(2)
+# ROOT.gPad.SetLeftMargin(0.15)
+# frame_mj_pdf.GetYaxis().SetTitleOffset(1.6)
+# frame_mj_pdf.Draw()
 
-c.SaveAs("plotTest.png")
+c.SaveAs("Gaus_"+options.mass_var+"_range65_105.png")
     
--- Fitter/partiallyMerged/makeRooDataset.py
+++ Fitter/partiallyMerged/makeRooDataset.py
@@ -1,7 +1,15 @@
+from optparse import OptionParser
 import ROOT
 #DON'T USE FUNCTIONS, WE BREAK LIKE MEN
 #Currently compatible with CMSSW_12_0
 #run with python3
+
+parser = OptionParser()
+parser.add_option('--mass_var', action="store",type="string",dest="mass_var",default="", help="jet mass grooming variable")
+(options, args) = parser.parse_args()
+
+# other mass_var options: sdB0, sdB1, sdB0Z0p05, sdB1Z0p05, sdB0Z0p15, sdB1Z0p15
+
 BinWidth = 5.
 in_mj_min = 50.
 in_mj_max = 130.
@@ -26,8 +34,9 @@ list_file_totalMC = list_file_STop_mc + list_file_VV_mc   + list_file_WJets_mc +
 #make workspace
 workspace = ROOT.RooWorkspace("workspace", "workspace")
 
+jet_mass = "SelectedJet_"+options.mass_var+"_mass"
 
-def makeDataset(list_file_in, label):
+def makeDataset(list_file_in, label, jet_mass):
 
     rrv_weight = ROOT.RooRealVar("rrv_weight","rrv_weight",0. ,10000000.)
     rdataset_mj = ROOT.RooDataSet("rdataset_"+label+"_mj","rdataset_"+label + "mj",ROOT.RooArgSet(rrv_mass_j,rrv_weight),ROOT.RooFit.WeightVar(rrv_weight) )
@@ -37,26 +46,27 @@ def makeDataset(list_file_in, label):
 
     #Loop through events
     treeIn = ROOT.TChain("Events") #initialize TChain to store events from all files of interest
+    treeIn.Print("SelectedJet*")
     for f in list_file_in:
         fileIn_name = ROOT.TString(file_Directory+f)  
         treeIn.Add(fileIn_name.Data())
         print("Using file: ", fileIn_name)
-            
+    print("Using mass variable: ", jet_mass)        
     print("N entries: ", treeIn.GetEntries())
         
     print("rrv_mass_j.getMax() = " ,rrv_mass_j.getMax())
     print("rrv_mass_j.getMin() = " ,rrv_mass_j.getMin())
     tmp_event_weight = 1.
     i = 0
-    testing = True        
+    
     for i in range(treeIn.GetEntries()):
-        if i % 5000 == 0: print("iEntry: ",i, " storing all")
+        if i % 5000 == 0: print("iEntry: ",i)
+        #print("iEntry: ", i)
         event = treeIn.GetEntry(i)
         #apply pt cut
-        if (treeIn.SelectedJet_pt < AK8_pt_min): continue
-        if (treeIn.SelectedJet_pt > AK8_pt_max): continue
+        if not (treeIn.SelectedJet_pt > AK8_pt_min): continue
+        if not (treeIn.SelectedJet_pt < AK8_pt_max): continue
         #apply mass cut
-        jet_mass = "SelectedJet_sdB0_mass" #soft drop mass var.
         if getattr(treeIn, jet_mass) > rrv_mass_j.getMax() and getattr(treeIn, jet_mass)< rrv_mass_j.getMin() : continue
         #get tau21 values
         #tau21 = treeIn.SelectedJet_tau21_ddt_retune # we don't use ddt right?
@@ -69,33 +79,40 @@ def makeDataset(list_file_in, label):
             lumiweight = 1.0
             tmp_scale_to_lumi = treeIn.eventweightlumi
             tmp_event_weight = tmp_scale_to_lumi
-              
         else:
             tmp_scale_to_lumi = 1.
             tmp_event_weight = 1.
         
-        if rrv_mass_j.getMin() < tmp_jet_mass < rrv_mass_j.getMax():
+#        print("tau21 used: ", tau21)
+#        print("temp_scale_to_lumi: ", tmp_scale_to_lumi, "treeIn.eventweightlumi: ", treeIn.eventweightlumi)
+
+        if (rrv_mass_j.getMin() < tmp_jet_mass < rrv_mass_j.getMax()):
             rrv_mass_j.setVal(tmp_jet_mass)
             rdataset_mj.add(ROOT.RooArgSet(rrv_mass_j), tmp_event_weight)
-        if (tau21 <= 0.35) and (rrv_mass_j.getMin() < tmp_jet_mass < rrv_mass_j.getMax()): #HP cut
+        if (tau21 <= 0.35) and tmp_jet_mass > rrv_mass_j.getMin() and tmp_jet_mass < rrv_mass_j.getMax(): #HP cut
             rrv_mass_j.setVal(tmp_jet_mass)
             rdataset_passtau21cut_mj.add(ROOT.RooArgSet(rrv_mass_j), tmp_event_weight)
+            # print("event ", i, " passed!")
+            # print("pt: ", treeIn.SelectedJet_pt, "jet mass: ", getattr(treeIn, jet_mass), "sdB0_mass", treeIn.SelectedJet_sdB0_mass)
         if (0.35 < tau21 <= 0.75) and (rrv_mass_j.getMin() < tmp_jet_mass < rrv_mass_j.getMax()): #LP cut
             rrv_mass_j.setVal(tmp_jet_mass)
             rdataset_failtau21cut_mj.add(ROOT.RooArgSet(rrv_mass_j), tmp_event_weight)
         if (tau21 > 0.75) and (rrv_mass_j.getMin() < tmp_jet_mass < rrv_mass_j.getMax()): #extreme fail
             rdataset_extremeFail_mj.add(ROOT.RooArgSet(rrv_mass_j), tmp_event_weight)
-    print("Number of pass events: ", rdataset_passtau21cut_mj.sumEntries())
-    print("Number of fail events: ", rdataset_failtau21cut_mj.sumEntries())
-    print("Total events passing pt and mass cut: ", rdataset_mj.sumEntries())
+    print("N events passing tau cut: ", rdataset_passtau21cut_mj.numEntries())
+    print("Sum of weights of pass events: ", rdataset_passtau21cut_mj.sumEntries())
+    print("Number of fail events: ", rdataset_failtau21cut_mj.numEntries())
+    print("Sum of weight of fail events: ", rdataset_failtau21cut_mj.sumEntries())
+    print("Total events passing pt and mass cut: ", rdataset_mj.numEntries())
+    print("Sum of weights passing pt and mass cut: ", rdataset_mj.sumEntries())
     getattr(workspace, "import")(rdataset_mj)
     getattr(workspace, "import")(rdataset_passtau21cut_mj)
     getattr(workspace, "import")(rdataset_failtau21cut_mj)
     getattr(workspace, "import")(rdataset_extremeFail_mj)
 #add dataSets to workspace for data and MC
-#makeDataset(list_file_totalMC, "totalMC")
-makeDataset(list_file_VV_mc, "VV")
-#makeDataset(list_file_data, "data")
+makeDataset(list_file_totalMC, "totalMC", jet_mass)
+#makeDataset(list_file_VV_mc, "VV")
+makeDataset(list_file_data, "data", jet_mass)
 
 workspace.Print()
-workspace.writeToFile("workspace_sdB0.root")
+workspace.writeToFile("workspace_"+options.mass_var+".root")
